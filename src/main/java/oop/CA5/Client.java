8332086ff655379e9b92a200c81656def5942464
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2024-04-18 15:25:38 +0100

    Client/server fix

--- src/main/java/oop/CA5/Client.java
+++ src/main/java/oop/CA5/Client.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client
 {
     /**
@@ -12,40 +14,47 @@ public class Client
      */
     public static void main(String[] args)
     {
-        final String SERVER_ADDRESS  = "localhost";
-        final int SERVER_PORT = 8888;
 
-        try(Socket socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader console = new BufferedReader(new InputStreamReader(System.in)))
+        Client client = new Client();
+        client.start();
+    }
+
+    public void start()
+    {
+        try(Socket socket = new Socket("localhost", 8888);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())))
         {
-            System.out.println("The client is running and has connected to the server.");
+            System.out.println("Client message: The Client is running and has connected to the server");
 
-            label:
-            while(true)
+            Scanner console = new Scanner(System.in);
+            String userRequest = "";
+
+            while (userRequest != "11")
             {
-                //Display menu:
                 System.out.println("1. Display Product by ID");
                 System.out.println("2. Display Vendor by ID");
                 System.out.println("3. Display Vendors selling chosen Product");
                 System.out.println("4. Display Products sold by chosen Vendor");
-                System.out.println("5. Display all Vendors");
-                System.out.println("6. Display all Products");
+                System.out.println("5. Display all Products");
+                System.out.println("6. Display all Vendors");
+                System.out.println("7. Display all Offers");
+                System.out.println("8. Add Product to an order");
+                System.out.println("9. DeleteProduct from the order");
+                System.out.println("10.Get Images List");
+                System.out.println("11. Exit");
                 System.out.println();
-                System.out.print("Choose an option: ");
+                System.out.println("Please enter a command: ");
 
-                String option = console.readLine();
+                userRequest = console.nextLine();
+                out.println(userRequest);
 
-                //Send user's option to the server
-                out.println(option);
-
-                switch (option)
+                switch (userRequest)
                 {
                     case "1":
                     {
                         System.out.print("Enter ID: ");
-                        String id = console.readLine();
+                        String id = console.next();
                         //Send ID to the server
                         out.println(id);
                         String pString = in.readLine();
@@ -55,7 +64,7 @@ public class Client
                     case "2":
                     {
                         System.out.print("Enter ID: ");
-                        String id = console.readLine();
+                        String id = console.next();
                         out.println(id);
                         String vString = in.readLine();
                         System.out.println("Response from the server: " + vString);
@@ -64,7 +73,7 @@ public class Client
                     case "3":
                     {
                         System.out.print("Enter ID: ");
-                        String id = console.readLine();
+                        String id = console.next();
                         out.println(id);
                         String vPString = in.readLine();
                         System.out.println("Response from the server: " + vPString);
@@ -73,21 +82,15 @@ public class Client
                     case "4":
                     {
                         System.out.print("Enter ID: ");
-                        String id = console.readLine();
+                        String id = console.next();
                         out.println(id);
                         String pVString = in.readLine();
                         System.out.println("Response from the server: " + pVString);
                         break;
                     }
                     case "5":
-                    {
-                        System.out.print("Enter ID: ");
-                        String id = console.readLine();
-                        out.println(id);
-                        String vPString = in.readLine();
-                        System.out.println("Response from the server: " + vPString);
+
                         break;
-                    }
                     case "6":
 
                         break;
@@ -108,9 +111,8 @@ public class Client
                         break;
                     default:
                         System.out.println("Invalid request or error in response");
-                        break label;
+                        break;
                 }
-
                 String jsonResponse = in.readLine();
                 if(jsonResponse != null)
                 {
@@ -118,16 +120,13 @@ public class Client
                     System.out.println(jsonResponse);
                 }
 
-                System.out.println();
+                console = new Scanner(System.in);
             }
-        }
-        catch (UnknownHostException e)
-        {
-            System.out.println(e);  // print out the exception
         }
         catch (IOException e)
         {
-            System.out.println(e);
+            System.out.println("Client message: IOException: " + e);
         }
+        System.out.println("Exiting client, but server may still be running.");
     }
 }
--- src/main/java/oop/CA5/Server.java
+++ src/main/java/oop/CA5/Server.java
@@ -8,8 +8,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import oop.CA5.DAOs.*;
-import oop.CA5.DTOs.Product;
-import oop.CA5.DTOs.Vendor;
 import oop.CA5.Exceptions.DaoException;
 
 public class Server
@@ -18,10 +16,102 @@ public class Server
      * Main author: Aleksandra Kail
      *
      */
-    public static void main(String[] args)
-    {
-        final int PORT = 8888; //port number to listen for
+    final int PORT = 8888; //port number to listen for
 
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+    public void start()
+    {
+
+        ServerSocket serverSocket =null;
+        Socket clientSocket =null;
+
+        try
+        {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server has started.");
+            int clientNumber = 0;  // a number sequentially allocated to each new client (for identification purposes here)
+
+            while (true)
+            {
+                System.out.println("Server: Listening/waiting for connections on port ..." + PORT);
+                clientSocket = serverSocket.accept();
+                clientNumber++;
+                System.out.println("Server: Listening for connections on port ..." + PORT);
+
+                System.out.println("Server: Client " + clientNumber + " has connected.");
+                System.out.println("Server: Port number of remote client: " + clientSocket.getPort());
+                System.out.println("Server: Port number of the socket used to talk with client " + clientSocket.getLocalPort());
+
+                // create a new ClientHandler for the requesting client, passing in the socket and client number,
+                // pass the handler into a new thread, and start the handler running in the thread.
+                Thread t = new Thread(new ClientHandler(clientSocket, clientNumber));
+                t.start();
+
+                System.out.println("Server: ClientHandler started in thread " + t.getName() + " for client " + clientNumber + ". ");
+
+            }
+        }
+        catch (IOException ex)
+        {
+            System.out.println(ex);
+        }
+        finally
+        {
+            try
+            {
+                if(clientSocket!=null)
+                    clientSocket.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println(e);
+            }
+            try
+            {
+                if(serverSocket!=null)
+                    serverSocket.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println(e);
+            }
+
+        }
+        System.out.println("Server: Server exiting, Goodbye!");
+    }
+}
+
+class ClientHandler implements Runnable   // each ClientHandler communicates with one Client
+{
+    BufferedReader socketReader;
+    PrintWriter socketWriter;
+    Socket clientSocket;
+    final int clientNumber;
+
+    // Constructor
+    public ClientHandler(Socket clientSocket, int clientNumber)
+    {
+        this.clientSocket = clientSocket;  // store socket for closing later
+        this.clientNumber = clientNumber;  // ID number that we are assigning to this client
+        try
+        {
+            // assign to fields
+            this.socketWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run ()
+    {
         ProductDaoInterface IProductDao = new MySqlProductDao();
         VendorDaoInterface IVendorDao = new MySqlVendorDao();
         ProductsVendorsDaoInterface IProductsVendorsDao = new MySqlProductsVendorsDao();
@@ -29,81 +119,79 @@ public class Server
 
         int id;
         String jsonString;
-
-        try (ServerSocket serverSocket = new ServerSocket(PORT))
+        String request;
+        try
         {
-            while (true)
+            while  ((request = socketReader.readLine()) != null)
             {
-                try ( Socket clientSocket = serverSocket.accept();
-                      // connection is made.
-                      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
+                System.out.println("Server: (ClientHandler): Read command from client " + clientNumber + ": " + request);
+                switch (request)
                 {
-                    System.out.println("Server started. A client has connected.");
-
-                    //Read client's request
-                    String option = in.readLine();
-                    System.out.println("Received option from client: " + option);
-
-                    switch (option)
-                    {
-                        case "1":
-                            id = Integer.parseInt(in.readLine());
-                            System.out.println(id);
-                            out.println("Display Product by ID: ");
-                            jsonString = jsonConverter.ConvertProductToJsonString(IProductDao.getProductById(id));
-                            out.println(jsonString);
-                            break;
-                        case "2":
-                            id = Integer.parseInt(in.readLine());
-                            System.out.println(id);
-                            out.println("Display Vendor by ID: ");
-                            jsonString = jsonConverter.ConvertProductToJsonString(IVendorDao.getVendorById(id));
-                            out.println(jsonString);
-                            break;
-                        case "3":
-                            id = Integer.parseInt(in.readLine());
-                            System.out.println(id);
-                            out.println("Display Vendors selling chosen Product: ");
-                            jsonString = jsonConverter.convertVendorListToJsonString(IProductsVendorsDao.getVendorsSellingProductId(id));
-                            out.println(jsonString);
-                            break;
-                        case "4":
-                            id = Integer.parseInt(in.readLine());
-                            System.out.println(id);
-                            out.println("Display Products sold by chosen Vendor: ");
-                            jsonString = jsonConverter.ConvertProductToJsonString(IProductsVendorsDao.getProductsSoldByVendorId(id));
-                            out.println(jsonString);
-                            break;
-                        case "5":
-                            break;
-                        case "6":
-                            break;
-                        case "7":
-                            break;
-                        case "8":
-                            break;
-                        case "9":
-                            break;
-                        case "10":
-                            break;
-                        case "11":
-                            break;
-                        default:
-                            out.println("Invalid option");
-                            break;
-                    }
-                    out.flush();
+                    case "1":
+                        id = Integer.parseInt(socketReader.readLine());
+                        System.out.println(id);
+                        socketWriter.println("Display Product by ID: ");
+                        jsonString = jsonConverter.ConvertProductToJsonString(IProductDao.getProductById(id));
+                        socketWriter.println(jsonString);
+                        break;
+                    case "2":
+                        id = Integer.parseInt(socketReader.readLine());
+                        System.out.println(id);
+                        socketWriter.println("Display Vendor by ID: ");
+                        jsonString = jsonConverter.ConvertProductToJsonString(IVendorDao.getVendorById(id));
+                        socketWriter.println(jsonString);
+                        break;
+                    case "3":
+                        id = Integer.parseInt(socketReader.readLine());
+                        System.out.println(id);
+                        socketWriter.println("Display Vendors selling chosen Product: ");
+                        jsonString = jsonConverter.convertVendorListToJsonString(IProductsVendorsDao.getVendorsSellingProductId(id));
+                        socketWriter.println(jsonString);
+                        break;
+                    case "4":
+                        id = Integer.parseInt(socketReader.readLine());
+                        System.out.println(id);
+                        socketWriter.println("Display Products sold by chosen Vendor: ");
+                        jsonString = jsonConverter.ConvertProductToJsonString(IProductsVendorsDao.getProductsSoldByVendorId(id));
+                        socketWriter.println(jsonString);
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        break;
+                    case "10":
+                        break;
+                    case "11":
+                        break;
+                    default:
+                        socketWriter.println("Invalid option");
+                        break;
                 }
             }
         }
-        catch (IOException e)
+        catch (IOException | DaoException ex)
         {
-            e.printStackTrace();
+            ex.printStackTrace();
         }
-        catch (DaoException e)
+        finally
         {
-            throw new RuntimeException(e);
+            this.socketWriter.close();
+            try
+            {
+                this.socketReader.close();
+                this.clientSocket.close();
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
         }
+        System.out.println("Server: (ClientHandler): Handler for Client " + clientNumber + " is terminating .....");
     }
 }
